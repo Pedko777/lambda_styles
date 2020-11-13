978db92c4602324b81097a49af3a2e6397f26987
Pedko777 <yuriipedko@gmail.com>
2020-11-13 12:31:22 +0200

    Fixed arusel styles for desktop

--- src/components/Body/NewSliderSection/NewSliderSection.js
+++ src/components/Body/NewSliderSection/NewSliderSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './NewSliderSection.module.scss';
 
 import { ReactComponent as Left } from '../../../ui/carousel/left.svg';
@@ -22,135 +22,134 @@ const NewSliderSection = () => {
     x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
     setActive(true);
   };
-  // useEffect(() => {
-  //   activeLeft ? setActiveRight(false) : setActiveRight(true);
-  //   activeRight ? setActiveLeft(false) : setActiveRight(true);
-  // }, [activeLeft, activeRight]);
+
   return (
     <section className={styles.sectionContainer}>
-      <div className={styles.content}>
-        <div className={styles.carousel}>
-          <div className={styles.slider}>
-            <div
-              className={styles.slide}
-              style={{ transform: `translateX(${x}%` }}
-            >
-              <img src={Orders} alt="slideImg" className={styles.slideImg} />
-            </div>
-            <div
-              className={styles.slide}
-              style={{ transform: `translateX(${x}%` }}
-            >
-              <img
-                src={OperationalCosts}
-                alt="slideImg"
-                className={styles.slideImg}
-              />
-            </div>
-            <div
-              className={styles.slide}
-              style={{ transform: `translateX(${x}%` }}
-            >
-              <img
-                src={Analytics}
-                alt="slideImg"
-                className={styles.slideImgLast}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.descriptions}>
-          <ul className={styles.btnWrapper}>
-            <li>
-              <button
-                className={!active ? styles.activeBtn : styles.btn}
-                onClick={prev}
-              >
-                <Left className={styles.img} />
-              </button>
-            </li>
-            <li>
-              <ul className={styles.paginationList}>
-                <li>
-                  <button
-                    className={
-                      x === 0
-                        ? styles.paginationBtnActive
-                        : styles.paginationBtn
-                    }
-                    onClick={() => {
-                      setX(0);
-                    }}
-                  ></button>
-                </li>
-                <li>
-                  <button
-                    className={
-                      x === -100
-                        ? styles.paginationBtnActive
-                        : styles.paginationBtn
-                    }
-                    onClick={() => {
-                      setX(-100);
-                    }}
-                  ></button>
-                </li>
-                <li>
-                  <button
-                    className={
-                      x === -200
-                        ? styles.paginationBtnActive
-                        : styles.paginationBtn
-                    }
-                    onClick={() => {
-                      setX(-200);
-                    }}
-                  ></button>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button
-                className={active ? styles.activeBtn : styles.btn}
-                onClick={next}
-              >
-                <Right className={styles.img} />
-              </button>
-            </li>
-          </ul>
-          <div className={styles.textCarousel}>
+      <div className={styles.contentWrap}>
+        <div className={styles.content}>
+          <div className={styles.carousel}>
             <div className={styles.slider}>
               <div
                 className={styles.slide}
                 style={{ transform: `translateX(${x}%` }}
               >
-                <h3 className={styles.sectionTitle}>
-                  All your orders in one place
-                </h3>
-                <p className={styles.descriprionText}>
-                  Say no more to chunky emails and messy WhatsApp messages.
-                  Receive and manage your orders in one platform
-                </p>
+                <img src={Orders} alt="slideImg" className={styles.slideImg} />
               </div>
               <div
                 className={styles.slide}
                 style={{ transform: `translateX(${x}%` }}
               >
-                <h3 className={styles.sectionTitle}>Some text</h3>
-                <p className={styles.descriprionText}>
-                  Say no more to chunky emails and messy WhatsApp messages.
-                  Receive and manage your orders in one platform
-                </p>
+                <img
+                  src={OperationalCosts}
+                  alt="slideImg"
+                  className={styles.slideImg}
+                />
               </div>
               <div
                 className={styles.slide}
                 style={{ transform: `translateX(${x}%` }}
               >
-                <h3 className={styles.sectionTitle}>Bla bla bla</h3>
-                <p className={styles.descriprionText}>
-                  Say no more to chunky emails and messy WhatsApp messages.
-                  Receive and manage your orders in one platform
-                </p>
+                <img
+                  src={Analytics}
+                  alt="slideImg"
+                  className={styles.slideImgLast}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.descriptions}>
+            <ul className={styles.btnWrapper}>
+              <li>
+                <button
+                  className={!active ? styles.activeBtn : styles.btn}
+                  onClick={prev}
+                >
+                  <Left className={styles.img} />
+                </button>
+              </li>
+              <li>
+                <ul className={styles.paginationList}>
+                  <li>
+                    <button
+                      className={
+                        x === 0
+                          ? styles.paginationBtnActive
+                          : styles.paginationBtn
+                      }
+                      onClick={() => {
+                        setX(0);
+                      }}
+                    ></button>
+                  </li>
+                  <li>
+                    <button
+                      className={
+                        x === -100
+                          ? styles.paginationBtnActive
+                          : styles.paginationBtn
+                      }
+                      onClick={() => {
+                        setX(-100);
+                      }}
+                    ></button>
+                  </li>
+                  <li>
+                    <button
+                      className={
+                        x === -200
+                          ? styles.paginationBtnActive
+                          : styles.paginationBtn
+                      }
+                      onClick={() => {
+                        setX(-200);
+                      }}
+                    ></button>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button
+                  className={active ? styles.activeBtn : styles.btn}
+                  onClick={next}
+                >
+                  <Right className={styles.img} />
+                </button>
+              </li>
+            </ul>
+            <div className={styles.textCarousel}>
+              <div className={styles.slider}>
+                <div
+                  className={styles.slide}
+                  style={{ transform: `translateX(${x}%` }}
+                >
+                  <h3 className={styles.sectionTitle}>
+                    All your orders in one place
+                  </h3>
+                  <p className={styles.descriprionText}>
+                    Say no more to chunky emails and messy WhatsApp messages.
+                    Receive and manage your orders in one platform
+                  </p>
+                </div>
+                <div
+                  className={styles.slide}
+                  style={{ transform: `translateX(${x}%` }}
+                >
+                  <h3 className={styles.sectionTitle}>Some text</h3>
+                  <p className={styles.descriprionText}>
+                    Say no more to chunky emails and messy WhatsApp messages.
+                    Receive and manage your orders in one platform
+                  </p>
+                </div>
+                <div
+                  className={styles.slide}
+                  style={{ transform: `translateX(${x}%` }}
+                >
+                  <h3 className={styles.sectionTitle}>Bla bla bla</h3>
+                  <p className={styles.descriprionText}>
+                    Say no more to chunky emails and messy WhatsApp messages.
+                    Receive and manage your orders in one platform
+                  </p>
+                </div>
               </div>
             </div>
           </div>
